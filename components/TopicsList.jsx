7bc819d1c16796390626b11a285b65a0918fa96a
kennethfazon <kennethfazon@thelewiscollege.edu.ph>
2025-01-04 19:27:03 +0800

    eyy

--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -4,7 +4,7 @@ import { HiPencilAlt } from "react-icons/hi";
 
 const getTopics = async () => {
   try {
-    const res = await fetch("https://your-app.vercel.app/api/topics", {
+    const res = await fetch("/api/topics", {
       cache: "no-store",
     });
 
